b900a8f5ab78000e0ab0e7065d20a505c7ad2d88
Tom Gidden <gid@starberry.tv>
2013-12-05 13:33:00 +0000

    Fixed with persistent configuration, passing config page via URL encoding rather than B64

--- GNUmakefile
+++ GNUmakefile
@@ -1,3 +1,5 @@
+build: build/beebwatch-2.pbw
+
 run: build/beebwatch-2.pbw
 	pebble install --logs --phone 192.168.7.67
 
@@ -6,11 +8,14 @@ install: build/beebwatch-2.pbw
 
 clean:
 	pebble clean
-	rm src/js/pebble-js-app.min.js
+	rm src/js/pebble-js-app.js
 
-build/beebwatch-2.pbw: src/js/pebble-js-app.min.js src/beebwatch.c
+build/beebwatch-2.pbw: src/js/pebble-js-app.js src/beebwatch.c appinfo.json
 	pebble build
 
-src/js/pebble-js-app.min.js: src/js/pebble-js-app.src.js src/js/configuration.html
-	sed -e 's/BASE64MARKER/'`base64 < src/js/configuration.html`'/' $< | /usr/local/share/npm/bin/uglifyjs -c -m > $@
+src/js/pebble-js-app.js: src/js/pebble-js-app.src.js src/js/configuration.html
+#	sed -e 's/URLENCODEMARKER/'`perl -pe's/([^-_.~A-Za-z0-9])/sprintf("%%%02X",ord($$1))/seg;' src/js/configuration.html`'/' $< > $@
+	sed -e 's/URLENCODEMARKER/'`perl -pe's/([^-_.~A-Za-z0-9])/sprintf("%%%02X",ord($$1))/seg;' src/js/configuration.html`'/' $< | /usr/local/share/npm/bin/uglifyjs -c -m > $@
+#	sed -e 's/BASE64MARKER/'`base64 < src/js/configuration.html`'/' $< | /usr/local/share/npm/bin/uglifyjs -c -m > $@
+#	sed -e 's/BASE64MARKER/'`base64 < src/js/configuration.html`'/' $< > $@
 
--- src/js/pebble-js-app.src.js
+++ src/js/pebble-js-app.src.js
@@ -1,27 +1,33 @@
-function save(j, key) {
-    var o = JSON.parse(j);
-    var d = {'1':o.sechand,'2':o.showtime,'3':o.showdate,'4':o.is24h};
-    try {
-        Pebble.sendAppMessage(d);
-        if(key) localStorage.setItem(key, j);
-    } catch (e) {}
-}
+/*global console:true, Pebble:true, window:true */
+var config = {};
 
-Pebble.addEventListener('appmessage', function(e) {
-    key = e.payload.action;
-    if (typeof(key) != 'undefined') {
-        var settings = localStorage.getItem('conf');
-        if (typeof(settings) == 'string')
-            save(settings);
+Pebble.addEventListener('ready', function () {
+    console.log('loaded');
+    // Load configuration from localStorage
+    var json = window.localStorage.getItem('beeb');
+    if (typeof json === 'string') {
+        config = JSON.parse(json);
     }
 });
 
-Pebble.addEventListener('showConfiguration', function(e) {
-    var o = Pebble.openURL('data:text/html;base64,BASE64MARKER');
+Pebble.addEventListener('appmessage', function (e) {
+    var key = e.payload.action;
+    if (key !== undefined) {
+        console.log('appmessage got ' + key + " " + JSON.stringify(e.payload));
+        Pebble.sendAppMessage({0:0});
+    }
 });
 
-Pebble.addEventListener('webviewclosed', function(e) {
-    if ((typeof(e.response) == 'string') && (e.response.length > 0)) {
-        save(e.response,'conf');
+Pebble.addEventListener('showConfiguration', function () {
+//    Pebble.openURL('data:text/html;base64,BASE64MARKER');
+    Pebble.openURL('data:text/html,URLENCODEMARKER'.replace("CONFDATA", encodeURI(JSON.stringify(config)), 'g'));
+});
+
+Pebble.addEventListener('webviewclosed', function (e) {
+    if ((typeof e.response === 'string') && (e.response.length > 0)) {
+        console.log("Sending " + e.response + " to Pebble");
+        config = JSON.parse(e.response);
+        window.localStorage.setItem('beeb', e.response);
+        Pebble.sendAppMessage(config);
     }
 });
